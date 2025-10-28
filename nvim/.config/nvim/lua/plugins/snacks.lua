447c5b01b1714dea98d5bb032ea39ab552302691
Felipe Oliveira <felipe_ods@outlook.com>
2025-10-27 22:34:35 -0300

    configurações gerais do neovim

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "snacks.nvim",
+    opts = {
+      indent = { enabled = false },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = true },
+      toggle = { map = LazyVim.safe_keymap_set },
+      dashboard = {
+        preset = {
+          header = [[ _______             ____   ____.__         
+ \      \   ____  ___\   \ /   /|__| _____  
+ /   |   \_/ __ \/  _ \   Y   / |  |/     \ 
+/    |    \  ___(  <_> )     /  |  |  Y Y  \
+\____|__  /\___  >____/ \___/   |__|__|_|  /
+        \/     \/                        \/ ]],
+        },
+      },
+    },
+  },
+}
