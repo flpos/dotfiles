0883d392ac0a22417221904b914e9d2d1424436e
Felipe <felipe.sousa@sulamerica.com.br>
2025-10-28 19:00:06 -0300

    awesome: ajusta atalhos de tamanho da janela master

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -345,10 +345,10 @@ globalkeys = mytable.join(
 	awful.key({ modkey, "Control" }, "r", awesome.restart, { description = "reload awesome", group = "awesome" }),
 	awful.key({ modkey, "Shift" }, "q", awesome.quit, { description = "quit awesome", group = "awesome" }),
 
-	awful.key({ modkey, altkey }, "l", function()
+	awful.key({ modkey }, "l", function()
 		awful.tag.incmwfact(0.05)
 	end, { description = "increase master width factor", group = "layout" }),
-	awful.key({ modkey, altkey }, "h", function()
+	awful.key({ modkey }, "h", function()
 		awful.tag.incmwfact(-0.05)
 	end, { description = "decrease master width factor", group = "layout" }),
 	awful.key({ modkey, "Shift" }, "h", function()
