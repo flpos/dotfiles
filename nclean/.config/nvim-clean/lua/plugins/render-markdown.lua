c248245adcddffc3b760b4d4c2911081644b4624
Felipe Oliveira <felipe_ods@outlook.com>
2026-01-04 00:23:00 -0300

    adiciona render markdown

---
+++ nclean/.config/nvim-clean/lua/plugins/render-markdown.lua
@@ -0,0 +1,10 @@
+vim.pack.add({
+  'https://github.com/nvim-treesitter/nvim-treesitter',
+  'https://github.com/nvim-mini/mini.nvim',
+  'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+})
+require('render-markdown').setup({})
+vim.pack.add({
+  'https://github.com/tadmccorkle/markdown.nvim'
+})
+require('markdown').setup()
